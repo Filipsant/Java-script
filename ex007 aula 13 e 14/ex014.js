f972f6742cdf8a828ed86b311f680b0391a6c828
Filipe Sant'Anna Santos <125302716+Filipsant@users.noreply.github.com>
2023-04-11 15:27:27 -0300

    aula 14

---
+++ ex007 aula 13 e 14/ex014.js
@@ -0,0 +1,36 @@
+console.log('vai começar...')
+for(var c = 1; c <= 6 ; c++ ){
+  console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+/*
+var c = 1
+
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c < 6)
+
+*/
+
+/*
+
+
+var c = 1
+
+while(c < 6){
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+/*
+console.log('Tudo bem ')
+console.log('Tudo bem ')
+console.log('Tudo bem ')
+console.log('Tudo bem ')
+console.log('Tudo bem ')
+*/
